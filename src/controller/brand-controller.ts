3d1b85bf319879f7b659b1fecab299e283f4292e
k9a8y6k5e <kaykekk8965@gmail.com>
2026-02-12 15:30:02 -0300

    add brand controller added

---
+++ src/controller/brand-controller.ts
@@ -0,0 +1,25 @@
+import type{Request, Response, NextFunction}from'express';
+import{ knex as db}from'../../database/knex-connection.js';
+
+interface BrandBody{
+    name :string;
+    nacionality :string;
+}
+
+async function brandAddController(req:Request,res:Response,next:NextFunction){
+    try{
+        const result =await _insertBrand(req.body as BrandBody);
+
+        res.status(201).json({work:true,data:result});
+    }
+    catch(err){next(err);}
+}
+
+async function _insertBrand(informations: BrandBody){
+    const[id]=await db('brand').insert({
+        brand_name :informations.name,
+        brand_nacionality :informations.nacionality
+    });
+
+    return{id : id, name :informations.name, nacionality :informations.nacionality};
+}
